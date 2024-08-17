d933d3a0f379265821fb17865029558bada0666e
Pankajjoshi11 <pankaj70451@gmail.com>
2024-08-17 15:38:23 +0530

    Initial commits

---
+++ backend/routes/postRoute.js
@@ -0,0 +1,11 @@
+//eg file
+
+
+const express = require('express');
+const { getPosts, addPost } = require('../controllers/postController');
+const router = express.Router();
+
+router.get('/posts', getPosts);
+router.post('/posts', addPost);
+
+module.exports = router;

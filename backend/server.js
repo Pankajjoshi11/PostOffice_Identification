5bcf7a2deaab75476fc75550d312e9979c8114de
Pankajjoshi11 <pankaj70451@gmail.com>
2024-08-27 01:29:45 +0530

    added post backend(Pankaj)

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoute');
+const postRoutes = require('./routes/postRoute'); // Import the post routes
 require('dotenv').config();
 
+// Connect to the database
 connectDB();
 
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json()); // Parses incoming JSON requests
 
-// Use the user routes
+// API Routes
 app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes); // Use the post routes
 
+
+// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
